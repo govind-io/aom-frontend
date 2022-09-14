93ad0520369d22bbdb1eb7a68f0bb98c04a64d26
Govind Sharma <gobind.sharma@refactorq.com>
2022-09-14 15:57:38 +0530

    speaker can connect to sfu

--- Components/Roundtables/LiveRt/Video/videoItem.js
+++ Components/Roundtables/LiveRt/Video/videoItem.js
@@ -1,98 +1,24 @@
-import { Grid, Skeleton, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useRef } from "react";
 
-export default function VideoItem({ peer }) {
-  //refs
+export default function VideoItem({ videoTrack }) {
   const videoRef = useRef();
 
-  //states
-  const [remoteStream, setRemoteStream] = useState();
-
-  //selectors
-  const participants = useSelector((state) => state.roundtable.participants);
-
   useEffect(() => {
-    if (peer.stream) {
-      return setRemoteStream(peer.stream);
-    }
-    peer.peer.on("stream", (stream) => {
-      console.log("stream");
-      setRemoteStream(stream);
-    });
-  }, []);
+    if (!videoTrack) return;
 
-  useEffect(() => {
-    if (!remoteStream) return;
+    videoRef.current.srcObject = videoTrack;
 
-    videoRef.current.srcObject = remoteStream;
-  }, [remoteStream]);
+    videoRef.current.play();
+  }, [videoTrack]);
 
   return (
-    <Grid
-      item
+    <video
+      ref={videoRef}
       style={{
-        height: "300px",
-        display: "flex",
-        justifyContent: "center",
-        width: participants?.length > 1 ? "50%" : "100%",
-        backgroundColor: "rgb(175 88 88)",
-        padding: "0px 20px",
+        width: "100%",
+        height: "100%",
+        objectFit: "containe",
       }}
-    >
-      {remoteStream ? (
-        <div
-          style={{
-            position: "relative",
-            maxWidth: "100%",
-            height: "295px",
-          }}
-        >
-          <video
-            style={{
-              width: "100%",
-              objectFit: "contain",
-              height: "100%",
-            }}
-            controls={false}
-            ref={videoRef}
-            autoPlay={true}
-          ></video>
-          <Box
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              position: "absolute",
-              bottom: "18px",
-            }}
-          >
-            <Typography
-              style={{
-                backgroundColor: "grey",
-                borderRadius: "5px",
-                padding: "5px",
-                fontSize: "12px",
-                fontWeight: "bold",
-                margin: "10px",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              {participants.find((elem) => elem.socketId === peer.peerId).name
-                .length > 12
-                ? participants
-                    .find((elem) => elem.socketId === peer.peerId)
-                    .name.substring(0, 9) + "..."
-                : participants.find((elem) => elem.socketId === peer.peerId)
-                    .name}{" "}
-            </Typography>
-          </Box>
-        </div>
-      ) : (
-        <Skeleton width="100%" height="300px" animation="wave" />
-      )}
-    </Grid>
+    ></video>
   );
 }
--- Components/Roundtables/LiveRt/Video/videoMain.js
+++ Components/Roundtables/LiveRt/Video/videoMain.js
@@ -1,3 +1,91 @@
+import { Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+import { socket } from "../../../../Utils/Configs/Socket";
+import VideoItem from "./videoItem";
+
 export default function VideoGrid() {
-  return <h1>Comming soon</h1>;
+  //states here
+  const [localTracks, setLocalTracks] = useState({ video: "", audio: "" });
+
+  const configuration = {
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  };
+
+  const StreamToServer = async () => {
+    const peerConnection = new RTCPeerConnection(configuration);
+
+    const localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream);
+    });
+
+    setLocalTracks({
+      audio: localStream.getAudioTracks()[0],
+      video: localStream.getVideoTracks()[0],
+    });
+
+    socket.on("answer", async ({ answer }) => {
+      const remoteDesc = new RTCSessionDescription(answer);
+
+      await peerConnection.setRemoteDescription(remoteDesc);
+    });
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    peerConnection.onicecandidate = (e) => {
+      if (e.candidate) {
+        socket.emit("addIceCandidate", {
+          newIceCandidate: e.candidate,
+        });
+      }
+    };
+
+    peerConnection.onconnectionstatechange = (e) => {
+      if (peerConnection.connectionState === "connected") {
+        console.log("connected to rtc server for sending ");
+      }
+    };
+
+    socket.emit("offer", { offer });
+
+    socket.on("addIceCandidateAnswer", async ({ newIceCandidate }) => {
+      await peerConnection.addIceCandidate(newIceCandidate);
+    });
+  };
+
+  const ViewStreamFromServer = async () => {
+    const peerConnection = new RTCPeerConnection(configuration);
+  };
+
+  console.log("tracks", { localTracks });
+
+  useEffect(() => {
+    socket.on("connected", StreamToServer);
+
+    return () => {
+      socket.off("connected", StreamToServer);
+    };
+  }, []);
+
+  return (
+    <>
+      <Grid container>
+        <Grid
+          item
+          xs={6}
+          style={{
+            borderRadius: "1px solid red",
+            height: "100px",
+          }}
+        >
+          {localTracks.video && <VideoItem videoTrack={localTracks.video} />}
+        </Grid>
+      </Grid>
+    </>
+  );
 }
